8d5f1b9d41a5c10ade9c713bcd639df8d25530f5
Christian von Lehe <kring.vonlehe@gmail.com>
2018-07-23 15:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments){
+  musicians.forEach(function(musicians,instruments){
+    console.log(musicians + ' plays ' + instruments);
+  });
+}
